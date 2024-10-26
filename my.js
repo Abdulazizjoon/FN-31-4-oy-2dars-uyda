159677c72cb4dc9e477c21a1699d7ac917a44d4e
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-10-26 19:14:32 +0500

    .

---
+++ my.js
@@ -0,0 +1,211 @@
+//odiy misollar
+//1-misol
+// let age = +prompt('yoshingizni kiriting')
+
+// if (age>16) {
+//     console.log('kirsangiz boladi');
+// } else {
+//     console.log('kirish taqiqlanadi');
+// }
+
+//2-misol
+// let number = +prompt("son kiriting");
+
+// if (number>0) {
+//   console.log(`${number} bu musbat son`);
+// } else {
+//   console.log(`${number} bu manfiy son`);
+// }
+
+//3-misol
+
+// let price = +prompt('baxoyingizni kiriting')
+
+// if (price>85) {
+//     console.log('siz alochisiz');
+// } else {
+//     console.log('sizning baxoyingiz yahshi emas');
+// }
+
+//4-misol
+
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+
+// if (son1>son2) {
+//   console.log("birinchi son katta");
+// } else {
+//   console.log("ikkingchi son katta");
+// }
+
+//5-misol
+
+// let parol = +prompt("parolni kirithing");
+
+// if (parol==12345) {
+//   console.log("kirish muvaffaqqiyatli amalga oshirildi");
+// } else {
+//   console.log("kirib bolmadi");
+// }
+
+//6-misol
+// let son = +prompt("son kiriting");
+
+// if (son %2==0) {
+//   console.log("juft son");
+// } else {
+//   console.log("kirib bolmadi");
+// }
+
+//7-misol
+// let price = +prompt("ballingizni kiriting");
+
+// if (price >= 50) {
+//   console.log("oqishga qabul qilindingiz");
+// } else {
+//   console.log("oqishga qabul qilinmadiz");
+// }
+
+//8-misol
+// let haftak=prompt('son kiriting')
+
+// if (haftak==1) {
+//     console.log('dushanba');
+// }else if (haftak==2) {
+//     console.log("seshanba");
+// }else if (haftak==3) {
+//     console.log("chorshanba");
+// }else if (haftak==4) {
+//     console.log("payshanba");
+// }else if (haftak==5) {
+//     console.log("juma");
+// }else if (haftak==6) {
+//     console.log("shanba dam olish kuni");
+// }else if (haftak==7) {
+//     console.log("yakshanba dam olish kuni");
+// } else {
+//     console.log('hatolik bor');
+// }
+//9-misol
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+
+// if (son1==son2) {
+//     console.log('ikkala son teng');
+// } else {
+//     console.log('ikkala son teng emas');
+// }
+
+//10-misol
+// let son1 = +prompt("soatni kiriting");
+// if (son1 >18) {
+//   console.log("kechki ovqat vaqti emas");
+// } else {
+//   console.log("kechki ovqat vaqti");
+// }
+
+//11-misol
+// let son1 = +prompt("daromadni kiriting");
+// if (son1 >1000) {
+//   console.log("yaxshi ish");
+// } else {
+//   console.log("yashi ish emas");
+// }
+
+//12-misol
+// let name = prompt("ismingizni kiriting");
+
+// console.log(`salom ${name}`);
+
+//13-misol
+// let son = +prompt('son kiriting')
+
+// if (son>100) {
+//     console.log('katta son');
+
+// } else {
+//     console.log('kichik son');
+
+// }
+//14-misol
+// let soat = +prompt('soat ti kiriting')
+
+// if (soat >= 8 && soat <= 18) {
+//   console.log("ish vaqti");
+// } else {
+//   console.log("ish vaqti emas");
+// }
+//15-misol
+// let oz = +prompt('ozlash tirish foyizini kirting')
+
+// if (oz>70) {
+//   console.log("ozlashtirishingiz yahshi");
+// } else {
+//   console.log("ozlashtirishingiz yahshi emas");
+// }
+
+//orta darajalik masala
+//1-masala
+// let son1 = +prompt("son kiriting");
+// let son2=+prompt('son kiriting')
+// let son3 = +prompt("son kiriting");
+// let max=son1+son2+son3
+// if (max>100) {
+//     console.log('Yigindisi katta ');
+// } else {
+//     console.log("Yigindisi kichik");
+// }
+
+//2-masala
+// let son1 = +prompt("ball");
+// let son2 = +prompt("ball");
+
+// if (son1 >= 60 && son2 >= 60) {
+//   console.log('imtihondan otdiz');
+// } else {
+//     console.log("imtihondan yiqildiz");
+// }
+
+//3-masala
+// let son1 = prompt("son");
+// let son2 = prompt("son");
+
+// if (son1%son2==0) {
+//     console.log('bir-biriga bolinadi');
+// } else {
+//     console.log('bir-biriga bolinmedi');
+// }
+
+//4-masala !hato tekshrmey qoyorin
+// let son1 = +prompt("son");
+// let son2 = +prompt("son");
+// let res;
+// if (son1%2==0) {
+//     res+'juft son'
+// }else if (son2%2==1) {
+//     res+'toq son'
+// }
+// else {
+//     console.log(false);
+// }
+// console.log();
+
+//5-masala
+// let age = +prompt('age')
+
+// let res=2024-age
+// if (res>=18) {
+//     console.log('siz voyaga yekansiz');
+// } else {
+//     console.log('siz voyaga yemagansiz');
+
+// }
+//8-masala
+let son1 = +prompt("son");
+let son2 = +prompt("son");
+
+let res = son1 + son2;
+
+if (res % 2 == 1) {
+    console.log('yigindisi tog');
+}
